bba4cb8a97f492f4718cf29c0de4904768614384
Aditya Neralkar <neralkarad15.it@coep.ac.in>
2018-04-02 01:46:46 +0530

    decode ethernet frame

---
+++ decode.py
@@ -0,0 +1,22 @@
+import socket
+import struct
+
+
+class Ethernet:
+
+    def __init__(self, raw_data):
+
+        dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+
+        self.dest_mac_addr = convert_mac_address(dest)
+        self.src_mac_addr = convert_mac_address(src)
+        self.proto = socket.htons(prototype)
+        self.data = raw_data[14:]
+        print(self.src_mac_addr, self.dest_mac_addr)
+
+
+
+def convert_mac_address(mac_raw):
+    byte_str = map('{:02x}'.format, mac_raw)
+    mac_addr = ':'.join(byte_str).upper()
+    return mac_addr
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import socket
 import sys
+from decode import Ethernet
 
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
         raw_data, addr = conn.recvfrom(65535)
-        print(raw_data)
+        internal_data = Ethernet(raw_data)
+
 
 if __name__ == '__main__':
     main()
