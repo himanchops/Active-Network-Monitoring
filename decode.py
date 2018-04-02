2c9691c90e104ef675efd244c994493a9b5df7a2
Aditya Neralkar <neralkarad15.it@coep.ac.in>
2018-04-02 14:02:36 +0530

    ipv4 decoded

--- decode.py
+++ decode.py
@@ -22,6 +22,18 @@ def convert_mac_address(mac_raw):
     return mac_addr
 
 
+
 class IPv4:
+
     def __init__(self, raw_data):
-        pass
+        version_header_length = raw_data[0]
+        self.version = version_header_length >> 4
+        self.header_length = (version_header_length & 15) * 4
+        self.ttl, self.proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', raw_data[:20])
+        self.src = self.convert_ip_address(src)
+        self.target = self.convert_ip_address(target)
+        self.data = raw_data[self.header_length:]
+
+    # Returns properly formatted IPV4 address
+    def convert_ip_address(self, addr):
+        return '.'.join(map(str, addr))
